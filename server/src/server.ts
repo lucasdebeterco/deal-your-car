ad6080984d008ad2cc810ed1d55124c2f433caa3
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-11-14 23:16:58 -0300

    Creating get posts route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 const app = express()
 
-app.get('/cars', (request, response) => {
-  return response.send({
-    "teste": 1234
-  })
+app.get('/posts', async (request, response) => {
+  const posts = await prisma.post.findMany()
+
+  return response.send(
+    posts
+  )
+})
+
+app.get('/states', async (request, response) => {
+  const states = await prisma.state.findMany()
+
+  return response.send(
+    states
+  )
 })
 
 app.listen(3333)
