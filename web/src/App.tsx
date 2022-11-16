f70314a5a42a3fbb43e1551c3d4cea021d67cdc0
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-11-15 23:21:10 -0300

    Creating posts component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,19 @@
+import { Post } from './components/post'
 import './styles/main.css'
 
 function App() {
   return (
+
     <div className='container mx-auto flex flex-col items-center my-20'>
-      <h1 className="flex align-middle text-3xl font-bold">Deal Your Car</h1>
+      <h1 className="flex align-middle text-4xl text-red-700 font-bold">Deal Your Car</h1>
+
+      <div className='w-full grid gap-5 grid-cols-4 mt-20'>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
   )
 }
---
+++ web/src/components/post.tsx
@@ -0,0 +1,11 @@
+export function Post() {
+  return (
+    <div className='p-4 border-solid border-[1px] rounded-lg border-gray-300 text-zinc-700'>
+        <div className="text-2xl font-semibold">model</div>
+        <div>brand</div>
+        <div>km</div>
+        
+        <div>price</div>
+    </div>
+  )
+}
