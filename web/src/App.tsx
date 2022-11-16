a750ebb2630bcea33e920a686eb7ddc45bddadf2
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-11-15 22:52:04 -0300

    Setting frontend with tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,10 @@
+import './styles/main.css'
+
 function App() {
   return (
-    <h1>Deal Your Car</h1>
+    <div className='container mx-auto flex flex-col items-center my-20'>
+      <h1 className="flex align-middle text-3xl font-bold">Deal Your Car</h1>
+    </div>
   )
 }
 
